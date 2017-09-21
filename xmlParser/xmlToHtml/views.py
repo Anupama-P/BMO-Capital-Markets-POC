35b6d30c10b790e0fa708722af5bf3f8ce12f740
Gaurav <glathwal@gmail.com>
2017-09-21 15:16:31 +0530

    parsed everything

--- xmlParser/xmlToHtml/views.py
+++ xmlParser/xmlToHtml/views.py
@@ -18,8 +18,20 @@ def xml_to_html_parser(request):
     team_members = member_parser(tree)
 
     company_data = company_data_parser(tree)
+    bmo_estimates = bmo_estimates_parser(tree)
+    consensus_estimates = consensus_estimates_parser(tree)
+    valuation = valuation_parser(tree)
+    eps = eps_parser(tree)
+    sidebar_stuff = sidebar_stuff_parser(tree)
+
+    income_statement = income_statement_parser(tree)
+    cash_flow_statement = cash_flow_statement_parser(tree)
+    balance_sheet = balance_sheet_parser(tree)
+    key_metrics = key_metrics_parser(tree)
 
     footer_stuff = footer_parser(tree)
+
+    footer_constant_stuff = footer_constant_stuff_parser(tree)
 
     return render_to_response(
         'xmlToHtml/output.html',
@@ -28,8 +40,18 @@ def xml_to_html_parser(request):
             'title': title,
             'header_data': header_data,
             'company_data': company_data,
+            'bmo_estimates' : bmo_estimates,
+            'consensus_estimates' : consensus_estimates,
+            'valuation' : valuation,
+            'eps' : eps,
+            'sidebar_stuff' : sidebar_stuff,
             'team_members': team_members,
-            'footer_stuff': footer_stuff
+            'income_statement' : income_statement,
+            'cash_flow_statement' : cash_flow_statement,
+            'balance_sheet' : balance_sheet,
+            'key_metrics' : key_metrics,
+            'footer_stuff': footer_stuff,
+            'footer_constant_stuff' : footer_constant_stuff
         })
 
 
@@ -150,3 +172,202 @@ def footer_parser(tree):
             data_dict['risks'] = elem.text.strip()
 
     return data_dict
+
+
+def bmo_estimates_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='FinancialModel'):
+		if elem.attrib['name'].find('BMO Estimates') > -1:
+			for values in elem:
+				data_dict[values.attrib['name']] = {}
+				for value in values:
+					if value.attrib['year'] == '2016' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2016'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2017' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2017'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2018' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2018'] = value.attrib['displayValue']
+
+	return data_dict
+
+
+def consensus_estimates_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='FinancialModel'):
+		if elem.attrib['name'].find('Consensus Estimates') > -1:
+			for values in elem:
+				data_dict[values.attrib['name']] = {}
+				for value in values:
+					if value.attrib['year'] == '2016' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2016'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2017' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2017'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2018' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2018'] = value.attrib['displayValue']
+
+	return data_dict
+
+def valuation_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='FinancialModel'):
+		if elem.attrib['name'].find('Valuation') > -1:
+			for values in elem:
+				data_dict[values.attrib['name']] = {}
+				for value in values:
+					if value.attrib['year'] == '2016' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2016'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2017' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2017'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2018' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2018'] = value.attrib['displayValue']
+
+	return data_dict
+
+def eps_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='TimesSeriesList'):
+		if elem.attrib['description'] == 'EPS':
+
+			data_dict['2016'] = {}
+			data_dict['2017'] = {}
+			data_dict['2018'] = {}
+
+			for fv in elem:
+				if fv.attrib['period'] == 'Q1' and fv.attrib['periodEnd'].split('-')[0] == '2016':
+					data_dict['2016']['Q1'] = fv[0].attrib['displayValue']
+				if fv.attrib['period'] == 'Q2' and fv.attrib['periodEnd'].split('-')[0] == '2016':
+					data_dict['2016']['Q2'] = fv[0].attrib['displayValue']
+				if fv.attrib['period'] == 'Q3' and fv.attrib['periodEnd'].split('-')[0] == '2016':
+					data_dict['2016']['Q3'] = fv[0].attrib['displayValue']
+				if fv.attrib['period'] == 'Q4' and fv.attrib['periodEnd'].split('-')[0] == '2016':
+					data_dict['2016']['Q4'] = fv[0].attrib['displayValue']
+
+				if fv.attrib['period'] == 'Q1' and fv.attrib['periodEnd'].split('-')[0] == '2017':
+					data_dict['2017']['Q1'] = fv[0].attrib['displayValue']
+				if fv.attrib['period'] == 'Q2' and fv.attrib['periodEnd'].split('-')[0] == '2017':
+					data_dict['2017']['Q2'] = fv[0].attrib['displayValue']
+				if fv.attrib['period'] == 'Q3' and fv.attrib['periodEnd'].split('-')[0] == '2017':
+					data_dict['2017']['Q3'] = fv[0].attrib['displayValue']
+				if fv.attrib['period'] == 'Q4' and fv.attrib['periodEnd'].split('-')[0] == '2017':
+					data_dict['2017']['Q4'] = fv[0].attrib['displayValue']
+
+				if fv.attrib['period'] == 'Q1' and fv.attrib['periodEnd'].split('-')[0] == '2018':
+					data_dict['2018']['Q1'] = fv[0].attrib['displayValue']
+				if fv.attrib['period'] == 'Q2' and fv.attrib['periodEnd'].split('-')[0] == '2018':
+					data_dict['2018']['Q2'] = fv[0].attrib['displayValue']
+				if fv.attrib['period'] == 'Q3' and fv.attrib['periodEnd'].split('-')[0] == '2018':
+					data_dict['2018']['Q3'] = fv[0].attrib['displayValue']
+				if fv.attrib['period'] == 'Q4' and fv.attrib['periodEnd'].split('-')[0] == '2018':
+					data_dict['2018']['Q4'] = fv[0].attrib['displayValue']
+
+	return data_dict
+
+def sidebar_stuff_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='BoilerPlate'):
+		if elem.attrib['name'] == 'Our Thesis':
+			data_dict['Our Thesis'] = elem.text.strip()
+		if elem.attrib['name'] == 'Valuation':
+			data_dict['Valuation'] = elem.text.strip()
+		if elem.attrib['name'] == 'Upside Scenario':
+			data_dict['Upside Scenario'] = elem.text.strip()
+		if elem.attrib['name'] == 'Downside Scenario':
+			data_dict['Downside Scenario'] = elem.text.strip()
+		if elem.attrib['name'] == 'Company Description':
+			data_dict['Company Description'] = elem.text.strip()
+			
+	return data_dict
+
+def income_statement_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='FinancialModel'):
+		if elem.attrib['name'].find('Income Statement') > -1:
+			for values in elem:
+				data_dict[values.attrib['name']] = {}
+				for value in values:
+					if value.attrib['year'] == '2016' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2016'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2017' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2017'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2018' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2018'] = value.attrib['displayValue']
+
+	return data_dict
+
+def cash_flow_statement_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='FinancialModel'):
+		if elem.attrib['name'].find('Cash Flow Statement') > -1:
+			for values in elem:
+				data_dict[values.attrib['name']] = {}
+				for value in values:
+					if value.attrib['year'] == '2016' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2016'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2017' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2017'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2018' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2018'] = value.attrib['displayValue']
+
+	return data_dict
+
+def balance_sheet_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='FinancialModel'):
+		if elem.attrib['name'].find('Balance Sheet') > -1:
+			for values in elem:
+				data_dict[values.attrib['name']] = {}
+				for value in values:
+					if value.attrib['year'] == '2016' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2016'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2017' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2017'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2018' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2018'] = value.attrib['displayValue']
+
+	return data_dict
+
+def key_metrics_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='FinancialModel'):
+		if elem.attrib['name'].find('Key Metrics') > -1:
+			for values in elem:
+				data_dict[values.attrib['name']] = {}
+				for value in values:
+					if value.attrib['year'] == '2016' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2016'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2017' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2017'] = value.attrib['displayValue']
+					if value.attrib['year'] == '2018' and value.tag == 'Value':
+						data_dict[values.attrib['name']]['2018'] = value.attrib['displayValue']
+
+	return data_dict
+
+def footer_constant_stuff_parser(tree):
+	data_dict = {}
+
+	for elem in tree.iter(tag='Disclaimer'):
+		if elem.attrib['code'] == 'DOR_LEGEND':
+			data_dict['ratings'] = elem.text.strip()
+
+	for elem in tree.iter(tag='Disclaimer'):
+		if elem.attrib['code'] == 'RATINGS':
+			data_dict['ratings_key'] = elem.text.strip()
+
+	for elem in tree.iter(tag='Disclaimer'):
+		if elem.attrib['code'] == 'OTHER_DIS':
+			data_dict['other_important_disclosures'] = elem.text.strip()
+
+	for elem in tree.iter(tag='Disclaimer'):
+		if elem.attrib['code'] == 'ADDL_INFO':
+			data_dict['actual_footer'] = elem.text.strip()
+
+	return data_dict
